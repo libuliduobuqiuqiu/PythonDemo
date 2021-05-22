317d02a7ebdf2ea37bc743c165cf87056514c7a4
libuliduobuqiuqiu <mbb929833@gmail.com>
2021-05-22 23:56:14 +0800

    Python设计模式：工厂方法，抽象工厂；

---
+++ patterns/Factory/AbstractFactory.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import abc
+
+
+class BYD:
+    def __repr__(self):
+        return "比亚迪"
+
+
+class BMW:
+    def __repr__(self):
+        return "宝马"
+
+
+class BMW_SUV:
+    def __repr__(self):
+        return "宝马SUV"
+
+
+class BYD_SUV:
+    def __repr__(self):
+        return "比亚迪SUV"
+
+
+class AbstractFactory(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def product_car(self):
+        pass
+
+    @abc.abstractmethod
+    def product_suv(self):
+        pass
+
+
+class BydFactory(AbstractFactory):
+    def __str__(self):
+        return "BYD工厂"
+
+    def product_car(self):
+        return BYD()
+
+    def product_suv(self):
+        return BYD_SUV()
+
+
+class BmwFactory(AbstractFactory):
+    def __str__(self):
+        return "BMW工厂"
+
+    def product_car(self):
+        return BMW()
+
+    def product_suv(self):
+        return BMW_SUV()
+
+
+if __name__ == "__main__":
+    byd_factory = BydFactory()
+    print(byd_factory.product_car())
+    print(byd_factory.product_suv())
+
+    bmw_factory = BmwFactory()
+    print(bmw_factory.product_car())
+    print(bmw_factory.product_suv())
--- patterns/Prototype/prototype.py
+++ patterns/Prototype/prototype.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import OrderedDict
+import copy
 
 
 class Book:
@@ -20,3 +21,33 @@ class Book:
         return my_str
 
 
+class Prototype:
+    def __init__(self):
+        self.objects = {}
+
+    def register(self, identifier, obj):
+        self.objects[identifier] = obj
+
+    def unregister(self, identifier):
+        del self.objects[identifier]
+
+    def clone(self, identifier, **attr):
+        found = self.objects.get(identifier)
+
+        if not found:
+            raise ValueError(f"找不到对应{identifier}对象")
+        obj = copy.deepcopy(found)
+        obj.__dict__.update(**attr)     # 自定义更新属性
+        return obj
+
+
+if __name__ == "__main__":
+    b = Book('The C Programming Language', ('Brian W. Kernighan', 'Dennis M.Ritchie'),
+             price=118, publisher='Prentice Hall', length=228, publication_date='1978-02-22',
+             tags=('C', 'programming', 'algorithms', 'data structures'))
+    print(b)
+    p = Prototype()
+    p.register("b1", b)
+    b2 = p.clone("b1", name='The C Programming Language (ANSI)', price=48.99, length=274,
+                 publication_date='1988-04-01', edition=2)
+    print(b2)
