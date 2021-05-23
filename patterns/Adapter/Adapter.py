710c56d816c80a12554e353694a1e9b43a041f84
不失者 <mbb929833@gmail.com>
2021-05-24 00:02:17 +0800

    Python设计模式：适配器模式

---
+++ patterns/Adapter/Adapter.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+
+class Computer:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"I'm a {self.name} computer."
+
+    def execute(self):
+        return "Execute a program"
+
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name}"
+
+    def speak(self):
+        return "Speak loudly."
+
+
+class Synthesizer:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"I am a {self.name} synthesizer."
+
+    def play(self):
+        return "Play a game"
+
+
+class Adapter:
+    def __init__(self, obj, adapter_method):
+        # 使用__dict__属性，实现适配器模式
+        self.obj = obj
+        self.__dict__.update(adapter_method)
+
+    def __str__(self):
+        return str(self.obj)
+
+
+if __name__ == "__main__":
+    objs = [Computer("XiaoMi")]
+    man = Human("Leijun")
+    synth = Synthesizer("Synth")
+
+    # 统一使用execute适配不同对象的方法，这样可以在不修改原有对象的基础上扩展功能
+    m_adapter = Adapter(man, {"execute": man.speak})
+    s_adapter = Adapter(synth, {"execute": synth.play})
+    objs.append(m_adapter)
+    objs.append(s_adapter)
+
+    for obj in objs:
+        print(f"{str(obj)}:{obj.execute()}")
---
+++ patterns/Adapter/Adapter2.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+
+class Computer:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"I'm a {self.name} computer."
+
+    def execute(self):
+        return "Execute a program"
+
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name}"
+
+    def speak(self):
+        return "Speak loudly."
+
+
+class Synthesizer:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"I am a {self.name} synthesizer."
+
+    def play(self):
+        return "Play a game"
+
+
+class HumanAdapter(Human):
+    def __init__(self, name):
+        """通过继承方式实现适配器模式"""
+        super().__init__(name)
+
+    def execute(self):
+        return self.speak()
+
+
+class SynthesizerAdapter(Synthesizer):
+    def __init__(self, name):
+        """通过继承方式实现适配器模式"""
+        super().__init__(name)
+
+    def execute(self):
+        return self.play()
+
+
+if __name__ == "__main__":
+    objs = [Computer("XiaoMi"), HumanAdapter("Leijun"), SynthesizerAdapter("Synthe")]
+
+    for obj in objs:
+        print(f"{str(obj)}:{obj.execute()}")
