d013d3e775b075a7b585faee356a96da3adfd779
libuliduobuqiuqiu <mbb929833@gmail.com>
2023-11-24 12:10:31 +0800

    #1 feat: 补充排序算法

---
+++ AlgorithmDemo/BubbleSortDemo/bubble_sort.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+"""
+    :date: 2023-11-24
+    :author: linshukai
+    :description: About buble sort algorithm
+"""
+
+
+def bubble_sort(unsort_list: list):
+    if len(unsort_list) < 1:
+        return
+
+    index = 0
+    while index < len(unsort_list) - 1:
+        tmpIndex = index + 1
+
+        while tmpIndex < len(unsort_list):
+            if unsort_list[index] > unsort_list[tmpIndex]:
+                unsort_list[index], unsort_list[tmpIndex] = unsort_list[tmpIndex], unsort_list[index]
+            tmpIndex += 1
+        index += 1
+
+
+if __name__ == "__main__":
+    unsort = [1, 23, 31, 2, 3, 1, 22, 11, 11, 23, 1, 2, 3]
+    bubble_sort(unsort)
+    print(unsort)
---
+++ AlgorithmDemo/SelectionSortDemo/selection_sort.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+"""
+    :date: 2023-11-24
+    :author: linshukai
+    :description: About Selection Sort Demo
+"""
+
+
+def selection_sort(unsort_list: list):
+    index = 0
+
+    while index < len(unsort_list) - 1:
+        exchange_index = index
+        tmp_index = index + 1
+
+        while tmp_index < len(unsort_list):
+            if unsort_list[exchange_index] > unsort_list[tmp_index]:
+                exchange_index = tmp_index
+            tmp_index += 1
+
+        if exchange_index != index:
+            unsort_list[index], unsort_list[exchange_index] = unsort_list[exchange_index], unsort_list[index]
+
+        index += 1
+
+
+if __name__ == "__main__":
+    unsort = [1, 23, 31, 2, 3, 1, 22, 11, 11, 23, 1, 2, 3]
+    selection_sort(unsort)
+    print(unsort)
