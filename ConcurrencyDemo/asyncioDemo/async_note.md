3b7895003b35dcc9c2dcefbab796eb0f1585d6d6
libuliduobuqiuqiu <mbb929833@gmail.com>
2023-12-29 17:16:02 +0800

    feat: update webosocket server.

---
+++ ConcurrencyDemo/asyncioDemo/async_note.md
@@ -0,0 +1,11 @@
+学习过程：
+1. ASGI协议
+2. uvicorn服务器(基于ASGI协议)
+3. uvicorn处理http请求、websocket请求
+4. websocket请求
+5. websockets模块处理websocket请求
+6. aioconsole异步处理input请求
+7. 聊天室(基于asyncio、websockets模块)
+8. FastAPI
+
+
--- ConcurrencyDemo/asyncioDemo/websocketsDemo/websockets_example.py
+++ ConcurrencyDemo/asyncioDemo/websocketsDemo/websockets_example.py
@@ -14,15 +14,25 @@ connections = set()
 
 # chat severice
 async def echo(websocket):
+    connections.add(websocket)
     async for message in websocket:
         print(f"Received from ({websocket}): ", message)
-        await websocket.send(message)
+        await broadcast(websocket, message)
+        # await websocket.send(message)
+
+
+async def broadcast(websocket, msg):
+    for client in connections:
+        if client != websocket:
+            print(client, websocket, "diff.")
+            await client.send(msg)
 
 
 async def main():
     print("Public Chat Room:")
-    async with serve(echo, "localhost", 80) as server:
+    async with serve(echo, "localhost", 80, open_timeout=None) as server:
         await server.wait_closed()
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
