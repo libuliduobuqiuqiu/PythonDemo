6adac182c28db962413d3bdb32d9e0374bdb5fd9
libuliduobuqiuqiu <mbb929833@gmail.com>
2024-01-09 00:37:50 +0800

    feat: update asgi framework to handle websocket.

--- ConcurrencyDemo/asyncioDemo/uvicornDemo/asgi_app.py
+++ ConcurrencyDemo/asyncioDemo/uvicornDemo/asgi_app.py
@@ -5,17 +5,20 @@
     :description: About Asgi Server
 """
 from handle_http_req import handle_http
+from handle_websocket_req import handle_websocket
 import asyncio
 import uvicorn
 
 
 async def app(scope, receive, send):
-    assert scope["type"] == "http"
-    await handle_http(scope, receive, send)
+    if scope["type"] == "http":
+        await handle_http(scope, receive, send)
+    elif scope["type"] == "websocket":
+        await handle_websocket(scope, receive, send)
 
 
 async def main():
-    config = uvicorn.Config("asgi_app:app", port=80, log_level="info")
+    config = uvicorn.Config("asgi_app:app", port=80, ws="websockets", log_level="info")
     server = uvicorn.Server(config=config)
     await server.serve()
 
--- ConcurrencyDemo/asyncioDemo/uvicornDemo/handle_websocket_req.py
+++ ConcurrencyDemo/asyncioDemo/uvicornDemo/handle_websocket_req.py
@@ -6,5 +6,25 @@
 """
 
 
-def handle_websocket(scope, receive, send):
+async def handle_websocket(scope, receive, send):
     assert scope["type"] == "websocket"
+    print(scope)
+
+    msg = await receive()
+
+    await send({"type": "websocket.accept", "headers": []})
+
+    receive_msg = await receive()
+    data = receive_msg["bytes"]
+
+    while data:
+        await send(
+            {
+                "type": "websocket.send",
+                "bytes": f"Server Got: {str(data)}".encode("utf-8"),
+            }
+        )
+        receive_msg = await receive()
+        data = receive_msg["bytes"]
+
+    await send({"type": "websocket.close", "headers": []})
