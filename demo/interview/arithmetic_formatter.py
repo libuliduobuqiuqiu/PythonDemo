48f47cbb07015e612febea712d880480d0d545cc
libuliduobuqiuqiu <mbb929833@gmail.com>
2024-05-28 17:45:57 +0800

    feat: add python free code camp project.

---
+++ demo/interview/arithmetic_formatter.py
@@ -0,0 +1,60 @@
+"""
+    :Date: 2024-5-28
+    :Author: linshukai
+    :Description: about arithmetic formatter
+"""
+
+
+def arithmetic_arranger(problems, show_answers=False):
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    first_column = []
+    second_column = []
+    third_column = []
+    result_column = []
+    for problem in problems:
+        tmp_list = problem.split()
+        number1 = tmp_list[0]
+        symbol = tmp_list[1]
+        number2 = tmp_list[2]
+
+        print(number1, number1.isdigit(), number2, number2.isdigit())
+        if not number1.isdigit() or not number2.isdigit():
+            return "Error: Numbers must only contain digits."
+
+        if len(number1) > 4 or len(number2) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        if symbol not in "+-":
+            return "Error: Operator must be '+' or '-'."
+
+        max_length = len(number1) if len(number1) > len(number2) else len(number2)
+        max_length += 2
+
+        first_column.append((max_length - len(number1)) * " " + number1)
+        second_column.append(symbol + (max_length - 1 - len(number2)) * " " + number2)
+        third_column.append(max_length * "-")
+
+        result = 0
+        if symbol == "+":
+            result = int(number1) + int(number2)
+        elif symbol == "-":
+            result = int(number1) - int(number2)
+        result_column.append((max_length - len(str(result))) * " " + str(result))
+
+    output = []
+    for i in [first_column, second_column, third_column, result_column]:
+        output.append("    ".join(i))
+
+    if show_answers:
+        return "\n".join(output)
+    else:
+        return "\n".join(output[:-1])
+
+
+if __name__ == "__main__":
+    result = arithmetic_arranger(["3801 - 2", "123 + 49"])
+    print(result)
+
+    result = arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"])
+    print(result)
---
+++ demo/interview/calculator.py
@@ -0,0 +1,82 @@
+"""
+    :Date: 2024-5-28
+    :Author: linshukai
+    :Description: About calculator
+"""
+
+
+def calculate_time(start_time, duration):
+    st_list = start_time.split()
+    st_begin = st_list[0]
+    st_end = st_list[1]
+
+    st_hour, st_minute = st_begin.split(":")
+    d_hour, d_minute = duration.split(":")
+
+    if not st_hour.isdigit() or not st_minute.isdigit():
+        return
+
+    if not d_hour.isdigit() or not d_minute.isdigit():
+        return
+
+    if st_end == "PM":
+        st_hour = int(st_hour) + 12
+
+    days = 0
+    next_hour = 0
+    minutes = int(st_minute) + int(d_minute)
+    if minutes >= 60:
+        next_hour += 1
+        minutes %= 60
+
+    hours = int(st_hour) + int(d_hour) + next_hour
+    if hours >= 24:
+        days = hours // 24
+        hours %= 24
+
+    res_time = ""
+    minutes = str(minutes) if minutes > 10 else f"0{minutes}"
+    if hours >= 12:
+        res_time = str(hours % 12) + ":" + minutes + " " + "PM"
+    else:
+        hours = str(hours) if hours != 0 else "12"
+        res_time = hours + ":" + minutes + " " + "AM"
+    return res_time, days
+
+
+def add_time(start, duration, weekend=""):
+    new_time, days = calculate_time(start, duration)
+
+    if weekend != "":
+        weekend_list = [
+            "monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+            "sunday",
+        ]
+        weekend = weekend.lower()
+        if weekend not in weekend_list:
+            return
+
+        weekend_index = weekend_list.index(weekend)
+        next_weekend_index = (weekend_index + days) % 7
+        weekend = weekend_list[next_weekend_index]
+        new_time += ", " + weekend.title()
+
+    days_suffix = ""
+    if days > 0:
+        days_suffix = f"({days} days later)" if days > 1 else "(next day)"
+        new_time += " " + days_suffix
+
+    return new_time
+
+
+if __name__ == "__main__":
+    print(add_time("3:30 PM", "2:12"))
+    print(add_time("11:55 AM", "3:12"))
+    print(add_time("2:59 AM", "24:00"))
+    print(add_time("2:59 AM", "24:00", "saturDay"))
+    print(add_time("11:59 PM", "24:05", "Wednesday"))
